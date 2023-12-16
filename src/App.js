1edfdfaf373e32bea34d8dcc9b6565e712d190fd
AryaGunawan <aryadipanegarag@gmail.com>
2023-12-16 10:25:06 +0700

    update header, movie, homepage, and search in file components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 // App.js
-import React from 'react';
-import HomePage from './components/home_page';// Make sure to update the import
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import HomePage from "./components/home_page"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Movie = ({ movie }) => {
+  // Menyimpan judul yang telah dipendek
+  const shortenedTitle = movie.Title.length > 18 ? movie.Title.slice(0, 25) + '...' : movie.Title;
+
   return (
-    <div className="movie">
-      <img src={movie.Poster} alt={`${movie.Title} Poster`} />
-      <div className="movie-infos">
-        <h2 className="movie-title">{movie.Title}</h2>
+    <div className="flex flex-col max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md">
+      <div>
+        <img
+          className="w-full h-64 object-cover"
+          src={movie.Poster}
+          alt={`${movie.Title} Poster`}
+        />
+      </div>
+      <div className="p-6">
+        <h2 className="font-bold text-xl mb-2">
+          {shortenedTitle}
+        </h2>
       </div>
     </div>
-
-    
   );
 };
 
